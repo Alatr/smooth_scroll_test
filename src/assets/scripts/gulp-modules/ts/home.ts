e69c58b73a08afb0f80c33262c823adc37a16aaf
Alatr <dimalukianovweb@gmail.com>
2020-09-01 16:41:54 +0300

    1

---
+++ src/assets/scripts/gulp-modules/ts/home.ts
@@ -0,0 +1,13 @@
+
+
+
+// import LocomotiveScroll from 'locomotive-scroll';
+// import { EventEmitter } from 'events'; 
+// import $ from 'jquery';
+/// <reference path ='../../../../typings.d.ts'/>
+
+import * as $ from 'jquery'
+import * as Locomotive from 'locomotive-scroll'
+console.log(Locomotive);
+console.log($('.breadcrumbs-item'));
+
---
+++ src/typings.d.ts
@@ -0,0 +1,8 @@
+// import { $ } from '../node_modules/jquery';
+
+// declare let $: $;
+// export default $;
+
+
+declare module "jquery";
+declare module "locomotive-scroll";
