f0ec989844eb49cbf8c58288695b847038a19cbb
Alatr <dimalukianovweb@gmail.com>
2020-09-02 16:26:12 +0300

    ll

--- src/assets/scripts/gulp-modules/ts/home.ts
+++ src/assets/scripts/gulp-modules/ts/home.ts
@@ -1,16 +1,16 @@
-
-
-
-// import LocomotiveScroll from 'locomotive-scroll';
-// import { EventEmitter } from 'events'; 
-// import $ from 'jquery';
+/* eslint-disable */
 /// <reference path ='../../../../typings.d.ts'/>
+/* eslint-enable */
 
-import $ =  require('JQuery');
+
+// console.log(sayHello('2'));
+
+
+import $ =  require('jquery');
 import LocomotiveScroll =  require('locomotive-scroll');
-// import * as m from "SomeModule";
-// import * as Locomotive from 'locomotive-scroll'
-// console.log(Locomotive);
+// // import * as m from "SomeModule";
+// // import * as Locomotive from 'locomotive-scroll'
+// // console.log(Locomotive);
 console.log($);
 console.log(LocomotiveScroll);
 
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,4 @@
 // In a .d.ts file or .ts file that is not a module:
-declare module 'JQuery' 
+declare module 'jquery' 
 declare module 'locomotive-scroll' 
+declare module 'module-alias/register' 
