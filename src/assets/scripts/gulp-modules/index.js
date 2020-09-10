1d994fcd979a9785bc0f6cd8519254c891a7c8e7
Alatr <dimalukianovweb@gmail.com>
2020-09-10 15:47:55 +0300

    skew

--- src/assets/scripts/gulp-modules/index.js
+++ src/assets/scripts/gulp-modules/index.js
@@ -35,6 +35,34 @@ const locoScroll = new LocomotiveScroll({
 * circle start
 */
 
+const secFoto2 = function() {
+	const img = '.section__2 .section__img img';
+	const text = '.section__2 h2';
+	const obj = {
+		// paused: true,
+	}
+	const tl = gsap.timeline(obj);
+		tl.fromTo(img, 5, {
+			css: {
+				transform: 'rotateY(0) rotateX(50deg) rotateZ(20deg)'
+			}
+		}, {
+				css: {
+					transform: ' rotateY(0) rotateX(50deg) rotateZ(-20deg)'
+				}
+		})
+		tl.fromTo(text, 5, {xPercent: -50}, {xPercent: 0}, '<')
+	return tl;
+};
+// circleTL().play()
+// createAnimationTool(circleTL)
+/*
+* circle end
+*/
+/*
+* circle start
+*/
+
 const circleTL = function() {
 	const circle = '.sec-circle';
 	const img = '.section__0 .section__img img';
@@ -93,9 +121,50 @@ ScrollTrigger.create({
 	scrub: true,
 	pin: true
 });
+ScrollTrigger.create({
+	trigger: ".section.section__2",
+	start: "top center",
+	// end: "+=1000",
+	// markers: true,
+	scroller: ".js-scroll-container",
+	animation: secFoto2(),
+	scrub: true,
+});
 
 // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
 ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
+
+
+
+
+
+
+
+let proxy = { skew: 0 },
+    skewSetter = gsap.quickSetter(".skewElem", "skewY", "deg"), // fast
+    clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees. 
+
+
+
+ScrollTrigger.create({
+	trigger: ".section__3",
+	start: "top bottom",
+	end: "+=1200",
+	scroller: ".js-scroll-container",
+	onUpdate: (self) => {
+		let skew = clamp(self.getVelocity() / -250);
+		// only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
+		if (Math.abs(skew) > Math.abs(proxy.skew)) {
+					console.log(self.getVelocity() / -300)
+	
+			proxy.skew = skew;
+			gsap.to(proxy, {skew: 0, duration: 0.8, ease: "power3", overwrite: true, onUpdate: () => skewSetter(proxy.skew)});
+		}
+	}
+});
+
+// make the right edge "stick" to the scroll bar. force3D: true improves performance
+gsap.set(".skewElem", {transformOrigin: "center", force3D: true});
